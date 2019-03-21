5621072c6c53ebdbeb97a5d63666dcfe010038d9
Collin Argo <collinargo@gmail.com>
2019-03-21 11:52:18 -0400

    change name to CalendApp

--- src/CalendApp.js
+++ src/CalendApp.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class CalendApp extends Component {
   render() {
     return (
       <div className="CalendApp">
@@ -12,4 +11,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default CalendApp;
