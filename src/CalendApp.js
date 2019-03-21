b3a029a9236471a2cb2c165be25917df6ad8f6f8
Collin Argo <collinargo@gmail.com>
2019-03-21 16:14:16 -0400

    setup

--- src/CalendApp.js
+++ src/CalendApp.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo'
 import { client } from './apollo/client.js'
-import './App.css';
+import './App.css'
 
 class CalendApp extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <div className="CalendApp">
-        
+        <p>hello</p>
         </div>
       </ApolloProvider>
     );
