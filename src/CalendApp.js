dc8e3d7fde22cd77b52e21911fec50a2a951dae1
Collin Argo <collinargo@gmail.com>
2019-03-21 11:50:14 -0400

    delete boilerplate

---
+++ src/CalendApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="CalendApp">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
