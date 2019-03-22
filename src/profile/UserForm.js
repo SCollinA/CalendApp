a288f5543f39b2cdfeb8c735a24e0529c4cf580c
Collin Argo <collinargo@gmail.com>
2019-03-22 14:04:32 -0400

    comment out mutation

--- src/profile/UserForm.js
+++ src/profile/UserForm.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { AppContext } from '../CalendApp'
+// import { Mutation } from 'react-apollo';
 
 export const UserForm = () => (
     <AppContext.Consumer>
         {({ user }) => (
+            // <Mutation mutation={UPDATE_USER}
+            //     update={(cache, { data, loading, error }) => {
+
+            //     }}
             <div className='UserForm'>
                 <p>user form</p>
             </div>
