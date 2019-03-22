3108719914f3e9bf4177a53eb6f37a649cc65cdd
Collin Argo <collinargo@gmail.com>
2019-03-22 16:17:32 -0400

    register new user

---
+++ src/profile/RegisterForm.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { AppContext } from '../CalendApp'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag'
+
+export const RegisterForm = ({ showLogin }) => (
+    <AppContext.Consumer>
+        {({ login }) => (
+            <Mutation mutation={REGISTER}
+                onCompleted={({ addUser: { token, user }}) => {
+                    localStorage.setItem('auth-token', token)
+                    login(user)
+                }}
+                onError={err => window.alert(err.message)}
+            >
+                {(registerMutation, { data, loading, error }) => (
+                    <form className='RegisterForm'
+                        onSubmit={event => {
+                            event.preventDefault()
+                            const user = {
+                                name: event.target.name.value,
+                                pwhash: event.target.password.value
+                            }
+                            registerMutation({
+                                variables: {
+                                    user
+                                },
+                            })
+                        }}
+                    >
+                        <label>name
+                            <input type='text' name='name' placeholder='name'/>
+                        </label>
+                        <label>password
+                            <input type='password' name='password' placeholder='password'/>
+                        </label>
+                        <label>password
+                            <input type='password' name='passwordConfirm' placeholder='confirm password'/>
+                        </label>
+                        <label>submit
+                            <input type='submit' value='register'/>
+                        </label>
+                        <input type='button' onClick={showLogin} value='login'/>
+                    </form>
+                )}
+            </Mutation>
+        )}
+    </AppContext.Consumer>
+)
+
+const REGISTER = gql`
+mutation Register($user: UserInput) {
+    addUser(user: $user) {
+        token
+        user {
+            _id
+            name
+            eventIds
+        }
+    }
+}
+`
