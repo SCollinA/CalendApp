d697b2fb8bc270a4277fd173c3cc4dbdfd96a921
Collin Argo <collinargo@gmail.com>
2019-03-29 13:31:56 -0400

    get show event form out of context

--- src/calendar/EventAddButton.js
+++ src/calendar/EventAddButton.js
@@ -2,58 +2,63 @@ import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import { AppContext } from '../CalendApp'
+import { CalContext } from './Calendar'
 import { GET_EVENTS } from './Week';
 
 export const EventAddButton = ({ timeStart, timeEnd }) => (
     <AppContext.Consumer>
         {({ user }) => (
-            <Mutation mutation={ADD_EVENT}
-                variables={{
-                    event: {
-                        hostId: user._id,
-                        name: 'new event',
-                        timeStart,
-                        timeEnd
-                    }
-                }}
-                update={(cache, { data: { addEvent }, loading, error}) => {
-                    const midnightToday = new Date(timeStart)
-                    midnightToday.setHours(0, 0, 0, 0)
-                    const getEvents = cache.readQuery({
-                        query: GET_EVENTS,
-                        variables: {
+            <CalContext.Consumer>
+                {({ showEventForm }) => (
+                    <Mutation mutation={ADD_EVENT}
+                        variables={{
                             event: {
                                 hostId: user._id,
-                                timeStart: midnightToday
+                                name: 'new event',
+                                timeStart,
+                                timeEnd
                             }
-                        }
-                    })
-                    cache.writeQuery({
-                        query: GET_EVENTS,
-                        variables: {
-                            event: {
-                                hostId: user._id,
-                                timeStart: midnightToday
-                            }
-                        },
-                        data: [
-                            ...getEvents,
-                            addEvent
-                        ]
-                    })
-                }}
-                onCompleted={({ addEvent }) => {
-                    showEventForm(addEvent)
-                }}
-            >
-                {(addEvent, { data, loading, error }) => (
-                    <div className='EventAddButton'
-                        onClick={() => addEvent()}
+                        }}
+                        update={(cache, { data: { addEvent }, loading, error}) => {
+                            const midnightToday = new Date(timeStart)
+                            midnightToday.setHours(0, 0, 0, 0)
+                            const getEvents = cache.readQuery({
+                                query: GET_EVENTS,
+                                variables: {
+                                    event: {
+                                        hostId: user._id,
+                                        timeStart: midnightToday
+                                    }
+                                }
+                            })
+                            cache.writeQuery({
+                                query: GET_EVENTS,
+                                variables: {
+                                    event: {
+                                        hostId: user._id,
+                                        timeStart: midnightToday
+                                    }
+                                },
+                                data: [
+                                    ...getEvents,
+                                    addEvent
+                                ]
+                            })
+                        }}
+                        onCompleted={({ addEvent }) => {
+                            showEventForm(addEvent)
+                        }}
                     >
-                        <p>free time - add event</p>
-                    </div>
+                        {(addEvent, { data, loading, error }) => (
+                            <div className='EventAddButton'
+                                onClick={() => addEvent()}
+                            >
+                                <p>free time - add event</p>
+                            </div>
+                        )}
+                    </Mutation>               
                 )}
-            </Mutation>
+            </CalContext.Consumer>
         )}
     </AppContext.Consumer>
 )
